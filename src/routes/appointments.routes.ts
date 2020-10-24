405eeeb7b52c5370351bccf1a2f83fbb709ab897
UrquizaN <urquiza-nobrega@outlook.com>
2020-10-23 21:13:18 -0300

    created date validation

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,18 +1,33 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
+  const parsedDate = startOfHour(parseISO(date));
+
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   };
+
+  const appointmentInSameDate = appointments.find( appointment => isEqual(appointment.date, parsedDate));
+
+  if(appointmentInSameDate){
+    return response.status(400).json({ message: 'This date is already booked! '});
+  }
 
   appointments.push(appointment);
 
