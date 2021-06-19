d8d2cb51262b783ecbfb065264cd0a38fd0588ee
Urquiza <urquiza-nobrega@outlook.com>
2021-06-18 22:36:40 -0300

    controllers implementation

--- src/modules/users/infra/http/routes/authentications.routes.ts
+++ src/modules/users/infra/http/routes/authentications.routes.ts
@@ -1,23 +1,9 @@
 import { Router } from 'express';
-import { container } from 'tsyringe';
-
-import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
+import AuthenticationsController from '../controllers/AuthenticationsController';
 
 const authenticationsRouter = Router();
+const authenticationsController = new AuthenticationsController();
 
-authenticationsRouter.post('/', async (request, response) => {
-  const { email, password } = request.body;
-
-  const authenticateUser = container.resolve(AuthenticateUserService);
-
-  const { user, token } = await authenticateUser.execute({
-    email,
-    password,
-  });
-
-  delete user.password;
-
-  return response.json({ user, token });
-});
+authenticationsRouter.post('/', authenticationsController.create);
 
 export default authenticationsRouter;
