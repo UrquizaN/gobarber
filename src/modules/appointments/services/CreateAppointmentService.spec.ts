aee043f77e396eb002897abcc617c1b5aec13222
Urquiza <urquiza-nobrega@outlook.com>
2021-07-01 22:10:24 -0300

    test of error when create appointment service

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,4 +1,5 @@
 import 'reflect-metadata';
+import AppError from '@shared/error/AppError';
 import CreateAppointmentService from './CreateAppointmentService';
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 
@@ -17,5 +18,27 @@ describe('CreateAppointment', () => {
 
     expect(appointment).toHaveProperty('id');
     expect(appointment.provider_id).toBe('123456');
+  });
+
+  it("shouldn't be able to create a new appointment in the same date", async () => {
+    const fakeAppointmentRepository = new FakeAppointmentsRepository();
+
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentRepository,
+    );
+
+    const appointmentDate = new Date(2020, 4, 10, 11);
+
+    await createAppointment.execute({
+      provider_id: '123456',
+      date: appointmentDate,
+    });
+
+    expect(
+      createAppointment.execute({
+        provider_id: '123456',
+        date: appointmentDate,
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
